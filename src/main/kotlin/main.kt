19de85aecbcf2a712a51c8b66a2e711f0a40256a
Nikolas S. Logemann <nikolasl@outlook.de>
2021-02-27 12:43:57 +0100

    Add command line argument for config file

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,25 @@
 package net.informatiger.ifw
 
+import net.informatiger.ifw.config.Config
 import net.informatiger.ifw.config.launchAllWatchers
 import net.informatiger.ifw.utils.ConfigUtils
 import net.informatiger.ifw.utils.LogUtils
+import java.io.File
 
-fun main() {
+fun main(args: Array<String>) {
+
+    val config: Config? =
+        if (args.isNotEmpty() && args[0].isNotEmpty()) {
+            val configFile = File(args[0])
+            if (configFile.exists())
+                ConfigUtils.loadConfig(configFile)
+            else
+                ConfigUtils.loadConfig(null)
+        } else {
+            ConfigUtils.loadConfig(null)
+        }
+
     val logger = LogUtils.getLogger("Main")
-    val config = ConfigUtils.loadConfig()
 
     config?.let {
         launchAllWatchers(it.watchers)
--- src/main/kotlin/utils/ConfigUtils.kt
+++ src/main/kotlin/utils/ConfigUtils.kt
@@ -1,7 +1,9 @@
 package net.informatiger.ifw.utils
 
 import com.sksamuel.hoplite.ConfigLoader
+import com.sksamuel.hoplite.PropertySource
 import net.informatiger.ifw.config.Config
+import java.io.File
 
 object ConfigUtils {
     private val logger = LogUtils.getLogger("ConfigLoader")
@@ -11,9 +13,19 @@ object ConfigUtils {
      *
      * @return [Config] or **null**
      */
-    fun loadConfig(): Config? {
+    fun loadConfig(configFile: File?): Config? {
         return try {
-            ConfigLoader().loadConfigOrThrow("/app.conf")
+            if (configFile != null) {
+                logger.info("Loading config from $configFile")
+                ConfigLoader.Builder()
+                    .addSource(PropertySource.file(configFile, false))
+                    // .addFileExtensionMapping("data", HoconParser)
+                    .build()
+                    .loadConfigOrThrow()
+            } else {
+                logger.info("Using default config")
+                ConfigLoader().loadConfigOrThrow("/app.conf")
+            }
         } catch (ex: Exception) {
             logger.error(ex.message)
             null
